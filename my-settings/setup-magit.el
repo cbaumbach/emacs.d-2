f512d800e2c82c28dc3929bb0ec0e81e974197f9
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-08-18 14:04:51 +0200

    Remove broken face-related code in setup-magit-el.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -6,16 +6,6 @@
 ;; Disable magit auto revert mode.
 (setq magit-auto-revert-mode nil)
 
-;; Remove some annoying highlighting.
-(set-face-attribute 'magit-section-title nil :inherit nil)
-(set-face-attribute 'magit-item-highlight nil :inherit nil)
-(set-face-attribute 'magit-diff-file-header nil :inherit nil)
-
-;; Improve readability of diff output.
-(set-face-foreground 'magit-diff-add "black")
-(set-face-foreground 'magit-diff-del "white")
-(set-face-foreground 'magit-diff-hunk-header "black")
-
 ;; Recenter screen after hiding details.
 (defun magit-show-level-2-all-recenter ()
   "Show only level 2 details and recenter screen."
