39c3bf9116aa7702b7a8880e80b6f8348e82287e
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-08-27 16:44:20 +0200

    Define personal C style.

--- my-settings/setup-ccmode.el
+++ my-settings/setup-ccmode.el
@@ -1,27 +1,23 @@
-;;; Set C indentation style.
-(setq c-default-style '((java-mode . "java")
-                        (awk-mode . "awk")
-                        (other . "stroustrup")))
+(defconst my-c-style
+  '("k&r"
+    (c-block-comment-prefix . "  ")
+    (c-basic-offset . 4)
+    (c-hanging-braces-alist . ((block-close before)
+                               (substatement-open after)))
+    (c-hanging-semi&comma-criteria . (c-semi&comma-no-newlines-before-nonblanks
+                                      c-semi&comma-inside-parenlist))
+    (c-offsets-alist . ((arglist-cont-nonempty . +)
+                        (arglist-close . 0)))))
 
-;;; Don't put an asterisk continuation lines of block comments.
-(setq-default c-block-comment-prefix "  ")
-
-;;; Harmonize tab settings.
-(defvaralias 'c-basic-offset 'tab-width)
-
-(c-set-offset 'arglist-cont-nonempty '+)
-(c-set-offset 'arglist-close 0)
+(c-add-style "personal" my-c-style)
 
 (defun my-c-mode-common-hook ()
+  (c-set-style "personal")
+  (c-toggle-syntactic-indentation 1)
   (c-toggle-electric-state 1)
   (c-toggle-auto-newline 1)
-  (c-toggle-hungry-state 1)
-  (c-toggle-syntactic-indentation 1))
+  (c-toggle-hungry-state 1))
 
 (add-hook 'c-mode-common-hook #'my-c-mode-common-hook)
-
-(setq c-hanging-semi&comma-criteria
-      (list #'c-semi&comma-no-newlines-before-nonblanks
-            #'c-semi&comma-inside-parenlist))
 
 (provide 'setup-ccmode)
