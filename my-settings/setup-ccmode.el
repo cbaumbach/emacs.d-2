b4653e72c5f53977e66aced84e0dda8cafa7850a
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-08-27 10:29:33 +0200

    Don't add newline after ; if next line non-blank.

--- my-settings/setup-ccmode.el
+++ my-settings/setup-ccmode.el
@@ -20,4 +20,8 @@
 
 (add-hook 'c-mode-common-hook #'my-c-mode-common-hook)
 
+(setq c-hanging-semi&comma-criteria
+      (list #'c-semi&comma-no-newlines-before-nonblanks
+            #'c-semi&comma-inside-parenlist))
+
 (provide 'setup-ccmode)
