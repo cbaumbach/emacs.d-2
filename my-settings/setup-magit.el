85a0d5160bc9b890b6f895e4aade9a92a11c7a76
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-08-27 02:03:43 +0200

    Make M-2 keybinding work with new magit version.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -7,15 +7,15 @@
 (setq magit-auto-revert-mode nil)
 
 ;; Recenter screen after hiding details.
-(defun magit-show-level-2-all-recenter ()
+(defun magit-section-show-level-2-all-recenter ()
   "Show only level 2 details and recenter screen."
   (interactive)
-  (magit-show-level-2-all)
+  (magit-section-show-level-2-all)
   (recenter-top-bottom)
   (previous-line 1))
 
 (add-hook 'magit-mode-hook
           #'(lambda ()
-              (local-set-key (kbd "M-2") 'magit-show-level-2-all-recenter)))
+              (local-set-key (kbd "M-2") 'magit-section-show-level-2-all-recenter)))
 
 (provide 'setup-magit)
