ad69620cea217d66519be392082640e771ec56d5
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-09-02 10:52:52 +0200

    Simplify magit diff face.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -6,6 +6,18 @@
 ;; Disable magit auto revert mode.
 (setq magit-auto-revert-mode nil)
 
+;;; Simplify magit diff face.
+(set-face-foreground 'magit-diff-added "blue")
+(set-face-background 'magit-diff-added "black")
+(set-face-foreground 'magit-diff-added-highlight "blue")
+(set-face-background 'magit-diff-added-highlight "black")
+(set-face-foreground 'magit-diff-removed "yellow")
+(set-face-background 'magit-diff-removed "black")
+(set-face-foreground 'magit-diff-removed-highlight "yellow")
+(set-face-background 'magit-diff-removed-highlight "black")
+(set-face-foreground 'magit-diff-hunk-heading "green")
+(set-face-foreground 'magit-diff-hunk-heading-highlight "green")
+
 ;; Recenter screen after hiding details.
 (defun magit-section-show-level-2-all-recenter ()
   "Show only level 2 details and recenter screen."
