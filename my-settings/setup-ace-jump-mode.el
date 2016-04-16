6e80d21d6c633e8c4ba20635e77dc810a430df54
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-04-16 23:32:36 +0200

    Use ace-jump-char-mode by default.

--- my-settings/setup-ace-jump-mode.el
+++ my-settings/setup-ace-jump-mode.el
@@ -5,4 +5,10 @@
 ;;; Search across all windows in a frame.
 (setq ace-jump-mode-scope 'frame)
 
+;;; Set order of submodes.
+(setq ace-jump-mode-submode-list
+      '(ace-jump-char-mode              ; C-c Space
+        ace-jump-word-mode              ; C-u C-c Space
+        ace-jump-line-mode))            ; C-u C-u C-c Space
+
 (provide 'setup-ace-jump-mode)
