c53a883628cd4e80fc9824b55784d930c2039bf3
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-05-05 19:25:48 +0200

    Activate dired-omit-mode from dired-x.

--- my-settings/setup-dired-mode.el
+++ my-settings/setup-dired-mode.el
@@ -1,3 +1,20 @@
-(add-hook 'dired-mode-hook #'(lambda () (dired-hide-details-mode 1)))
+(add-hook 'dired-load-hook
+          #'(lambda ()
+              (setq dired-guess-shell-gnutar "tar")
+              (load "dired-x")))
+
+(add-hook 'dired-mode-hook
+          #'(lambda ()
+              (dired-omit-mode 1)
+              (dired-hide-details-mode 1)))
+
+(autoload 'dired-jump "dired-x"
+  "Jump to Dired buffer corresponding to current buffer." t)
+
+(autoload 'dired-jump-other-window "dired-x"
+  "Like \\[dired-jump] (dired-jump) but in other window." t)
+
+(define-key global-map "\C-x\C-j" 'dired-jump)
+(define-key global-map "\C-x4\C-j" 'dired-jump-other-window)
 
 (provide 'setup-dired-mode)
