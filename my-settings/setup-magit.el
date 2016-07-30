71176b855a42793af7f36ff9e478739c387b9978
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-07-30 14:47:37 +0200

    Use advice to center screen after hiding details.

--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -18,16 +18,40 @@
 (set-face-background 'magit-diff-context-highlight "black")
 (set-face-foreground 'magit-section-highlight "yellow")
 
-;; Recenter screen after hiding details.
-(defun magit-section-show-level-2-all-recenter ()
-  "Show only level 2 details and recenter screen."
-  (interactive)
-  (magit-section-show-level-2-all)
-  (recenter-top-bottom)
-  (previous-line 1))
+;;; Recenter screen after hiding details.
+;;;
+;;; EXCURSION: When advising OLDFUN with FUNCTION, where FUNCTION is
+;;; interactive, the composed function inherits the interactive
+;;; specification of FUNCTION.  The composed function for an :after
+;;; advice is conceptually equivalent to the following lambda:
+;;;
+;;;   (lambda (&rest r) (prog1 (apply OLDFUN r) (apply FUNCTION r)))
+;;;
+;;; In particular, both OLDFUN and FUNCTION will be called with the
+;;; same arguments.  If the advised (composed) function is called
+;;; interactively and FUNCTION's interactive specification implies a
+;;; different number of arguments than OLDFUN we will get an error of
+;;; type wrong-number-of-arguments.
+;;;
+;;; (defun foo ()
+;;;   (interactive))
+;;;
+;;; (defun bar (&optional arg)
+;;;   (interactive "P"))
+;;;
+;;; (advice-add 'foo :after 'bar)
+;;;
+;;; (foo)                            ; works
+;;; (call-interactively 'foo)        ; wrong-number-of-arguments error
+;;;
+;;; Therefore in order to advise magit-section-show-level-2-all, which
+;;; takes no arguments, using recenter-top-bottom, which takes one
+;;; optional argument, we have to wrap the latter in a lambda that
+;;; takes no arguments.
 
-(add-hook 'magit-mode-hook
-          #'(lambda ()
-              (local-set-key (kbd "M-2") 'magit-section-show-level-2-all-recenter)))
+(defun magit-recenter ()
+  (recenter-top-bottom))
+
+(advice-add 'magit-section-show-level-2-all :after 'magit-recenter)
 
 (provide 'setup-magit)
