544423a173739d985a1d58a9eee81f09584e3e7e
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-09-21 15:29:26 +0200

    Bind <up>/<down> to org-[previous|next]-slide.

--- my-settings/setup-org-mode.el
+++ my-settings/setup-org-mode.el
@@ -17,6 +17,22 @@
 
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
+;;; Functions for using top-level trees as slides.
+(defun org-next-slide ()
+  (interactive)
+  (org-change-slide 1))
+
+(defun org-previous-slide ()
+  (interactive)
+  (org-change-slide -1))
+
+(defun org-change-slide (arg)
+  (widen)
+  (if (> arg 0)
+      (outline-next-heading)
+    (outline-previous-heading))
+  (org-narrow-to-subtree))
+
 (defun org-toggle-skip-confirm-babel-evaluate ()
   "Toggle flag to bypass the need for confirmation when
 evaluating source blocks."
@@ -30,7 +46,9 @@ evaluating source blocks."
 (add-hook 'org-mode-hook
           #'(lambda ()
               (setq-local org-skip-confirm-babel-evaluate nil)
-              (local-set-key (kbd "C-c M-s") 'org-toggle-skip-confirm-babel-evaluate)))
+              (local-set-key (kbd "C-c M-s") 'org-toggle-skip-confirm-babel-evaluate)
+              (local-set-key (kbd "<down>") 'org-next-slide)
+              (local-set-key (kbd "<up>") 'org-previous-slide)))
 
 (setq org-confirm-babel-evaluate
       #'(lambda (lang body)
