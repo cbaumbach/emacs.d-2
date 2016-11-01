62b66f2b911bb8407eeee9552eccfb1fda6a4239
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-01 01:06:07 +0100

    Load polymode only when load-polymode is called.

--- my-settings/setup-polymode.el
+++ my-settings/setup-polymode.el
@@ -1,7 +1,8 @@
-(add-to-list 'auto-mode-alist '("\\.md$" . poly-markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.Rmd$" . poly-markdown+r-mode))
-
-(require 'poly-R)
-(require 'poly-markdown)
+(defun load-polymode ()
+  (interactive)
+  (add-to-list 'auto-mode-alist '("\\.md$" . poly-markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rmd$" . poly-markdown+r-mode))
+  (require 'poly-R)
+  (require 'poly-markdown))
 
 (provide 'setup-polymode)
