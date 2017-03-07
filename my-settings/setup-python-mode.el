cc1f389af67534647ba73898d459c899b5108189
Clemens Baumbach <clemens.baumbach@eniano.com>
2017-03-07 18:32:07 +0100

    Delete in steps of 4 only if there are 4 spaces

--- my-settings/setup-python-mode.el
+++ my-settings/setup-python-mode.el
@@ -2,10 +2,12 @@
   (interactive)
   (insert "    "))
 
-(defun cb/delete-backward-4-characters-in-string ()
+(defun cb/delete-backward-4-spaces-in-string ()
   (interactive)
-  (let ((start-of-string (nth 8 (syntax-ppss))))
-    (delete-char (- (min 4 (- (point) start-of-string 1))))))
+  (if (looking-back "    ")
+      (let ((start-of-string (nth 8 (syntax-ppss))))
+        (delete-char (- (min 4 (- (point) start-of-string 1)))))
+    (delete-char -1)))
 
 (defun cb/newline-and-indent-to-first-nonblank ()
   (interactive)
@@ -21,7 +23,7 @@
               (cb/define-context-key python-mode-map
                                      (kbd "DEL")
                                      (when (cb/inside-a-multiline-string-p)
-                                       #'cb/delete-backward-4-characters-in-string))
+                                       #'cb/delete-backward-4-spaces-in-string))
               (cb/define-context-key python-mode-map
                                      (kbd "RET")
                                      (when (cb/inside-a-multiline-string-p)
