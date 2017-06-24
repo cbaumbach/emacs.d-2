d0c60cf3133ffb6947219a9e6415c35411e63d9f
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-06-24 21:41:23 +0200

    Have ack command prompt for directory

--- my-settings/setup-ack.el
+++ my-settings/setup-ack.el
@@ -3,8 +3,11 @@
 (defcustom ack-command "ack --with-filename --nofilter --nogroup "
   "Command-line used for `ack' command")
 
-(defun ack (command-args)
-  (interactive (list (read-shell-command "Pattern: " nil 'grep-history)))
-  (compilation-start (concat ack-command command-args) 'grep-mode))
+(defun ack (directory pattern)
+  (interactive
+   (list
+    (read-directory-name "Directory: ")
+    (read-string "Pattern: " nil 'grep-history)))
+  (compilation-start (format "%s %s %s" ack-command pattern directory) 'grep-mode))
 
 (provide 'setup-ack)
