23722fd80315e02adb5c329173052e2bedd9c57f
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-10-15 18:56:50 +0200

    Refactor key bindings file

--- my-settings/setup-keybindings.el
+++ my-settings/setup-keybindings.el
@@ -10,22 +10,11 @@
 (defun cb/other-window () (interactive) (other-window 1))
 (defun cb/other-frame () (interactive) (other-frame 1))
 
-(global-set-key-with-transient-map
- (kbd "C-x o") 'cb/other-window
- ("o" 'cb/other-window))
+(global-set-key-with-transient-map (kbd "C-x o") 'cb/other-window ("o" 'cb/other-window))
+(global-set-key-with-transient-map (kbd "C-x 5 o") 'cb/other-frame ("o" 'cb/other-frame))
 
-(global-set-key-with-transient-map
- (kbd "C-x 5 o") 'cb/other-frame
- ("o" 'cb/other-frame))
-
-;;; Add autoloads for some functions before binding keys to them.
-(autoload 'copy-from-above-command "misc" nil 'interactive)
-
+(global-set-key (kbd "M-=") 'er/expand-region) ; M-= expand, Meta - M-= contract, M-0 M-= reset
 (global-set-key (kbd "M-x") 'ido-M-x)
-
-;;; Type M-= to expand, Meta - M-= to contract, M-0 M-= to reset.
-(global-set-key (kbd "M-=") 'er/expand-region)
-
 (global-set-key (kbd "C-c C-SPC") 'ace-jump-mode)
 (global-set-key (kbd "<f5>") 'slime-selector)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
@@ -33,6 +22,7 @@
 (global-set-key (kbd "C-x ra") 'append-to-register)
 (global-set-key (kbd "C-x rF") 'copy-file-name-to-register)
 (global-set-key (kbd "C-w") 'delete-or-kill-region)
+(autoload 'copy-from-above-command "misc" nil 'interactive)
 (global-set-key (kbd "C-h C-SPC") 'copy-from-above-command)
 (global-set-key (kbd "C-M-y") 'copy-1-from-above)
 (global-set-key (kbd "M-z") 'zap-to-or-up-to-char)
