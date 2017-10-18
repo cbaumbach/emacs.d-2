6ec277e48430335284eff8ee1c6a40fe5b4ace7f
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-10-18 08:46:20 +0200

    Bind keys to paredit commands

--- my-settings/setup-paredit-mode.el
+++ my-settings/setup-paredit-mode.el
@@ -23,10 +23,22 @@
 
 (eval-after-load "paredit"
   '(progn
-     (define-key paredit-mode-map (kbd "C-c C-s") 'paredit-forward-slurp-sexp)
-     (define-key paredit-mode-map (kbd "C-c C-b") 'paredit-forward-barf-sexp)
-     (define-key paredit-mode-map (kbd "C-c s") 'paredit-backward-slurp-sexp)
-     (define-key paredit-mode-map (kbd "C-c b") 'paredit-backward-barf-sexp)
+     (define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round)
+     (define-key paredit-mode-map (kbd "M-s") 'paredit-splice-sexp)
+     (define-key paredit-mode-map (kbd "M-r") 'paredit-raise-sexp)
+     (define-key paredit-mode-map (kbd "M-?") 'paredit-convolute-sexp)
+     (define-key paredit-mode-map (kbd "M-S") 'paredit-split-sexp)
+     (define-key paredit-mode-map (kbd "M-J") 'paredit-join-sexps)
+     (define-key paredit-mode-map (kbd "s-<") 'paredit-splice-sexp-killing-backward)
+     (define-key paredit-mode-map (kbd "s->") 'paredit-splice-sexp-killing-forward)
+     (define-key paredit-mode-map (kbd "s-)") 'paredit-forward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "s-(") 'paredit-backward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "s-]") 'paredit-forward-barf-sexp)
+     (define-key paredit-mode-map (kbd "s-[") 'paredit-backward-barf-sexp)
+     (define-key paredit-mode-map (kbd "C-M-u") 'paredit-backward-up)
+     (define-key paredit-mode-map (kbd "C-M-d") 'paredit-forward-down)
+     (define-key paredit-mode-map (kbd "C-M-p") 'paredit-backward-down)
+     (define-key paredit-mode-map (kbd "C-M-n") 'paredit-forward-up)
      (define-key paredit-mode-map (kbd "DEL") 'paredit-backward-delete-or-delete-region)))
 
 (defun paredit-backward-delete-or-delete-region (&optional n)
