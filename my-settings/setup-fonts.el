e7a559baf5dad88f806fc12880cb428cfc224f33
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-11-03 11:48:29 +0100

    Use Consolas size 14 on Windows machines

--- my-settings/setup-fonts.el
+++ my-settings/setup-fonts.el
@@ -1,6 +1,13 @@
-(when (member "Unifont" (font-family-list))
-  (add-to-list 'initial-frame-alist '(font . "Unifont-12"))
-  (add-to-list 'default-frame-alist '(font . "Unifont-12")))
+(let* ((font-family-list (font-family-list))
+       (font (cond ((and (string-equal system-type "windows-nt")
+                         (member "Consolas" font-family-list))
+                    "Consolas-14")
+                   ((member "Unifont" font-family-list)
+                    "Unifont-12")
+                   (t nil))))
+  (when font
+    (add-to-list 'initial-frame-alist `(font . ,font))
+    (add-to-list 'default-frame-alist `(font . ,font))))
 
 ;;; Disable bold font.
 (mapc #'(lambda (face)
