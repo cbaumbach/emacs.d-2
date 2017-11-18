46af8a59216c4b09b52aa04e8d3924f6cbc54547
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-11-18 16:45:47 +0100

    Speed up ace jump mode by making it case-sensitive

--- my-settings/setup-ace-jump-mode.el
+++ my-settings/setup-ace-jump-mode.el
@@ -1,11 +1,8 @@
 (require 'ace-jump-mode)
 
 (set-face-foreground 'ace-jump-face-foreground "green")
-
-;;; Search across all windows in a frame.
 (setq ace-jump-mode-scope 'frame)
-
-;;; Set order of submodes.
+(setq ace-jump-mode-case-fold nil)
 (setq ace-jump-mode-submode-list
       '(ace-jump-char-mode              ; C-c Space
         ace-jump-word-mode              ; C-u C-c Space
