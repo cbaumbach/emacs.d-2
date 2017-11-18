df6a96bbfa1e23ee2954c98d719de12c81d6cd34
Clemens Baumbach <clemens.baumbach@gmx.net>
2017-11-18 16:51:29 +0100

    Refactor setup-appearance.el

--- my-settings/setup-appearance.el
+++ my-settings/setup-appearance.el
@@ -1,48 +1,19 @@
-;;; Inhibit loading default init file.
 (setq inhibit-default-init t)
-
-;;; Explicitly set foreground and background colors in ms windows.
 (when (string-equal system-type "windows-nt")
   (set-background-color "black")
   (set-foreground-color "white"))
-
-;;; Inhibit startup screen.
 (setq inhibit-startup-screen t)
-
-;;; Turn off the menu bar.
 (menu-bar-mode -1)
-
-;;; Turn off scroll bar.
-(if (fboundp 'scroll-bar-mode)
+(when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
-
-;;; Turn off tool bar.
-(if (fboundp 'tool-bar-mode)
+(when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
-
-;;; Truncate lines.
 (setq-default truncate-lines t)
-
-;;; Use visual bell.
 (setq visible-bell t)
-
-;;; Turn on font lock mode globally.
 (global-font-lock-mode t)
-
-;;; Use less syntax highlighting in selected modes and maximum
-;;; decoration otherwise.
-(setq font-lock-maximum-decoration '((cperl-mode . 1) (t . t)))
-
-;;; Always show matching parentheses.
 (show-paren-mode t)
-
-;;; Set text width to 70 columns.
 (setq fill-column 70)
-
-;;; Show column number.
 (setq-default column-number-mode t)
-
-;;; Display time in mode line.
 (display-time)
 
 (provide 'setup-appearance)
