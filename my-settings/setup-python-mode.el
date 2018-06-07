b16611ebf6bd9313dc4ef5ac8271863088d5c8d0
Clemens Baumbach <clemens.baumbach@gmx.net>
2018-06-07 12:05:46 +0200

    Add cb/run-django-shell function

--- my-settings/setup-python-mode.el
+++ my-settings/setup-python-mode.el
@@ -10,4 +10,23 @@
   (interactive)
   (setq python-shell-virtualenv-path nil))
 
+(defun cb/run-django-shell (venv manage.py)
+  "Run django's python shell in inferior-python-mode.
+
+I had to comment out the following lines in django's \"shell\"
+management command found in django/core/management/commands/shell.py.
+
+    if sys.platform != 'win32' and select.select([sys.stdin], [], [], 0)[0]:
+        exec(sys.stdin.read())
+        return
+
+Otherwise the django shell would hang waiting for input from stdin."
+  (interactive "DVirtual environment: \nfPath to manage.py: ")
+  (cb/python-set-virtualenv venv)
+  (setq python-shell-interpreter-args (concat manage.py " shell"))
+  (let ((pwd default-directory))
+    (cd (file-name-directory manage.py))
+    (call-interactively 'run-python)
+    (cd pwd)))
+
 (provide 'setup-python-mode)
