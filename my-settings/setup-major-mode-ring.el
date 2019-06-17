c8108b4fd03ed67d887bb0cc01277292b3bd4ac6
Clemens Baumbach <clemens.baumbach@eniano.com>
2019-06-17 10:47:09 +0200

    Facilitate noninteractive use of major-mode-ring

--- my-settings/setup-major-mode-ring.el
+++ my-settings/setup-major-mode-ring.el
@@ -7,34 +7,37 @@
 ;;; the ring's head element.  If the current major mode is not already
 ;;; in the ring, we insert it.
 
-(defun cb/ensure-major-mode-in-ring ()
-  (if (not (ring-member cb/major-mode-ring major-mode))
-      (ring-insert cb/major-mode-ring major-mode)))
+(defun cb/ensure-major-mode-in-ring (mode)
+  (if (not (ring-member cb/major-mode-ring mode))
+      (ring-insert cb/major-mode-ring mode)))
 
-(defun cb/major-mode-ring-add ()
+(defun cb/major-mode-ring-add (&optional mode)
   "Add current major mode to ring"
   (interactive)
-  (cb/ensure-major-mode-in-ring)
-  (princ major-mode))
+  (let ((mode (or mode major-mode)))
+    (cb/ensure-major-mode-in-ring mode))
+  (when (not mode)
+    (princ major-mode)))
 
 (defun cb/major-mode-ring-next ()
   "Change to next major mode"
   (interactive)
-  (cb/ensure-major-mode-in-ring)
+  (cb/ensure-major-mode-in-ring major-mode)
   (funcall (ring-next cb/major-mode-ring major-mode))
   (princ major-mode))
 
 (defun cb/major-mode-ring-previous ()
   "Change to previous major mode"
   (interactive)
-  (cb/ensure-major-mode-in-ring)
+  (cb/ensure-major-mode-in-ring major-mode)
   (funcall (ring-previous cb/major-mode-ring major-mode))
   (princ major-mode))
 
-(defun cb/major-mode-ring-remove ()
+(defun cb/major-mode-ring-remove (&optional mode)
   "Remove current major mode from ring"
   (interactive)
-  (let ((index (ring-member cb/major-mode-ring major-mode)))
+  (let* ((mode (or mode major-mode))
+         (index (ring-member cb/major-mode-ring mode)))
     (if index
         (progn
           (ring-remove cb/major-mode-ring index)
@@ -44,7 +47,7 @@
 (defun cb/major-mode-ring-show ()
   "Show all major modes in ring"
   (interactive)
-  (cb/ensure-major-mode-in-ring)
+  (cb/ensure-major-mode-in-ring major-mode)
   (let ((start (ring-member cb/major-mode-ring major-mode))
         (result))
     (dotimes (index (ring-length cb/major-mode-ring) (princ result))
