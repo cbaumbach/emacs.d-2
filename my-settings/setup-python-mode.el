2e89c0186d47983ecae3b32fc033aad18fc0aab4
Clemens Baumbach <clemens.baumbach@gmx.net>
2019-08-08 15:59:15 +0200

    Bind C-c C-n to new cb/python-shell-send-line

--- my-settings/setup-python-mode.el
+++ my-settings/setup-python-mode.el
@@ -36,4 +36,28 @@ Older django versions omitted the \"and not sys.stdin.isatty()\" part."
           #'(lambda ()
               (toggle-truncate-lines -1)))
 
+(defun cb/python-shell-send-line ()
+  "Copy rest of current line to inferior Python process and
+advance one line."
+  (interactive)
+  (let ((line (buffer-substring (point) (line-end-position))))
+    (python-shell-with-shell-buffer
+      (goto-char (point-max))
+      (insert line)
+      (comint-send-input)))
+  (let ((column (current-column)))
+    (forward-line)
+    (move-to-column column)))
+
+(defun cb/python-shell-send-empty-line ()
+  "Send empty line to inferior Python process."
+  (interactive)
+  (python-shell-with-shell-buffer
+    (comint-send-input)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-n") 'cb/python-shell-send-line)
+            (local-set-key (kbd "C-c RET") 'cb/python-shell-send-empty-line)))
+
 (provide 'setup-python-mode)
