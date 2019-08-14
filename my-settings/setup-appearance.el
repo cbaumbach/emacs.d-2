74e22ae0a3dd77cd328128e21c6d5f4d0abadee1
Clemens Baumbach <clemens.baumbach@gmx.net>
2019-08-15 00:42:21 +0200

    Explain how to fix theme colors in terminal emacs

--- my-settings/setup-appearance.el
+++ my-settings/setup-appearance.el
@@ -1,5 +1,22 @@
 (setq cb/color-theme 'dracula)
 (add-hook 'after-init-hook (lambda () (load-theme cb/color-theme t)))
+
+;;; For some unknown reason the dracula theme does not apply fully to
+;;; the magit-status buffer unless I reload it.  Without reloading,
+;;; the magit-diff-removed-highlight and magit-diff-added-highlight
+;;; faces will not have their default red/green foreground with light
+;;; red/green background color but will have a white foreground with
+;;; purple/teal background.
+(add-hook 'magit-status-mode-hook
+          (lambda () (load-theme cb/color-theme t)) t)
+
+;;; To get color themes to display correctly in terminal emacs when
+;;; connecting via PuTTY, use "xterm-256color" as the terminal type
+;;; string to under Connection, Data, and add the following alias to
+;;; ~/.bashrc
+;;;
+;;;     alias emacs='TERM=xterm-256color emacs'
+
 (setq inhibit-default-init t)
 (when (display-graphic-p)
   (add-to-list 'default-frame-alist '(foreground-color . "white"))
--- my-settings/setup-magit.el
+++ my-settings/setup-magit.el
@@ -2,13 +2,4 @@
 
 (setq magit-auto-revert-mode nil)
 
-;;; For some unknown reason the dracula theme does not apply fully to
-;;; the magit-status buffer unless I reload it.  Without reloading,
-;;; the magit-diff-removed-highlight and magit-diff-added-highlight
-;;; faces will not have their default red/green foreground with light
-;;; red/green background color but will have a white foreground with
-;;; purple/teal background.
-(add-hook 'magit-status-mode-hook
-          (lambda () (load-theme cb/color-theme t)) t)
-
 (provide 'setup-magit)
